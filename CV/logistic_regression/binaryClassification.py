748b498a51fca61b367f62764e75902ba3233f13
ChenXi-Hu <70460287+ChenXi-Hu@users.noreply.github.com>
2022-11-11 21:04:51 +0800

    Add files via upload

---
+++ CV/logistic_regression/binaryClassification.py
@@ -0,0 +1,16 @@
+from sklearn import linear_model
+
+X = [[20, 3], [23, 7], [31, 10], [42, 13], [50, 7], [60, 5], [28, 8]]
+
+y = [0, 1, 1, 1, 0, 0, 0] 
+
+lr = linear_model.LogisticRegression()
+lr.fit(X, y)
+
+testX = [[28, 8]]
+
+label = lr.predict(testX)
+print("predicted label = ", label)
+
+prob = lr.predict_proba(testX)
+print("probability = ", prob)
